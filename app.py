5f79488f0d47e00dc39a63229d8a00fcc259ab13
Siyki <isabeta.mar@gmail.com>
2025-05-02 00:49:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 st.markdown("""
     <style>
         html, body, .stApp {
-            background-color: #B3BAC4;
+            background-color: #23BAC4;
             color: #E0E0E0;
             font-family: 'Segoe UI', sans-serif;
         }
